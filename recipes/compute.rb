afbad78f708b5676fce9dc07aa8ddf0c0b94ffd3
Chaman Kang <chaman.kang@kt.com>
2013-04-12 15:12:41 +0900

    add feature to set quantum_default_private_network option in nova.conf

--- recipes/compute.rb
+++ recipes/compute.rb
@@ -6,7 +6,16 @@
 chef_gem "chef-rewind"
 require 'chef/rewind'
 
+# Define our own nova_conf provider and rewind nova_conf resource to use it
+# to add quantum_default_private_network option into the nova.conf file.
+include_recipe "ktc-nova::provider_nova_conf"
 include_recipe "nova::compute"
+rewind :nova_conf => "/etc/nova/nova.conf" do
+  provider Chef::Provider::KtcNovaConf
+  cookbook_name "ktc-nova"
+end
+
+# Rewind nova-compute.conf template to use the "lb" config source
 if node["quantum"]["plugin"] == "lb"
   rewind :template => "/etc/nova/nova-compute.conf" do
     source "folsom/nova-compute.conf.erb"
@@ -24,4 +33,3 @@ template "/usr/share/pyshared/nova/network/quantumv2/api.py" do
   notifies :restart, resources(:service => "nova-compute"), :immediately
   only_if { ::Chef::Recipe::Patch.check_package_version("nova-compute","2012.2.1+stable-20121212-a99a802e-0ubuntu1.4~cloud0",node) }
 end
-
