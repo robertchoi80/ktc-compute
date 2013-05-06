22511e04c6f7a610fbb55a4c1853e934bdf67617
Chaman <chaman.kang@kt.com>
2013-05-06 16:18:03 +0900

    update nova-compute patch in the compute recipe

--- recipes/compute.rb
+++ recipes/compute.rb
@@ -34,11 +34,14 @@ end
 
 # apply fixes for Alpha-1: location based IP assignment
 include_recipe "osops-utils"
-template "/usr/share/pyshared/nova/network/quantumv2/api.py" do
-  source "ktc-patches/api.py.2012.2.1+stable-20121212-a99a802e-0ubuntu1.4~cloud0"
-  owner "root"
-  group "root"
-  mode "0644"
-  notifies :restart, resources(:service => "nova-compute"), :immediately
-  only_if { ::Chef::Recipe::Patch.check_package_version("nova-compute","2012.2.1+stable-20121212-a99a802e-0ubuntu1.4~cloud0",node) }
+%w{ 2012.2.1+stable-20121212-a99a802e-0ubuntu1.4~cloud0 2012.2.3-0ubuntu2~cloud0 }.each do |version|
+  if ::Chef::Recipe::Patch.check_package_version("nova-compute",version,node)
+    template "/usr/share/pyshared/nova/network/quantumv2/api.py" do
+      source "ktc-patches/api.py.#{version}"
+      owner "root"
+      group "root"
+      mode "0644"
+      notifies :restart, resources(:service => "nova-compute"), :immediately
+    end
+  end
 end
