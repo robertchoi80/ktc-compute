75482cdce4f0f5041f58f8363c99ef93b479c344
Chaman <chaman.kang@kt.com>
2013-05-14 14:08:58 +0900

    update the compute recipe to patch manager.py

--- recipes/compute.rb
+++ recipes/compute.rb
@@ -24,12 +24,21 @@ if node["quantum"]["plugin"] == "lb"
   end
 end
 
-# apply fixes for Alpha-1: location based IP assignment
+# apply fixes for nova-compute
 include_recipe "osops-utils"
 %w{ 2012.2.1+stable-20121212-a99a802e-0ubuntu1.4~cloud0 2012.2.3-0ubuntu2~cloud0 }.each do |version|
   if ::Chef::Recipe::Patch.check_package_version("nova-compute",version,node)
     template "/usr/share/pyshared/nova/network/quantumv2/api.py" do
       source "ktc-patches/api.py.#{version}"
+      owner "root"
+      group "root"
+      mode "0644"
+      notifies :restart, resources(:service => "nova-compute"), :immediately
+    end
+  end
+  if ::Chef::Recipe::Patch.check_package_version("nova-compute",version,node)
+    template "/usr/share/pyshared/nova/network/quantumv2/manager.py" do
+      source "ktc-patches/manager.py.#{version}"
       owner "root"
       group "root"
       mode "0644"
