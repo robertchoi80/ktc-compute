1bf50d6e89eafd9ccb6bb0fc18b69020d6f1be70
sstrato <sstrato.kong@gmail.com>
2013-05-15 16:23:16 +0900

    UCLOUDNG-208 : quantum api in nova changed

--- recipes/compute.rb
+++ recipes/compute.rb
@@ -26,7 +26,7 @@ end
 
 # apply fixes for nova-compute
 include_recipe "osops-utils"
-%w{ 2012.2.1+stable-20121212-a99a802e-0ubuntu1.4~cloud0 2012.2.3-0ubuntu2~cloud0 }.each do |version|
+%w{ 1:2013.1-0ubuntu2~cloud1}.each do |version|
   if ::Chef::Recipe::Patch.check_package_version("nova-compute",version,node)
     template "/usr/share/pyshared/nova/network/quantumv2/api.py" do
       source "ktc-patches/api.py.#{version}"
@@ -36,13 +36,13 @@ include_recipe "osops-utils"
       notifies :restart, resources(:service => "nova-compute"), :immediately
     end
   end
-  if ::Chef::Recipe::Patch.check_package_version("nova-compute",version,node)
-    template "/usr/share/pyshared/nova/compute/manager.py" do
-      source "ktc-patches/manager.py.#{version}"
-      owner "root"
-      group "root"
-      mode "0644"
-      notifies :restart, resources(:service => "nova-compute"), :immediately
-    end
-  end
+#  if ::Chef::Recipe::Patch.check_package_version("nova-compute",version,node)
+#    template "/usr/share/pyshared/nova/compute/manager.py" do
+#      source "ktc-patches/manager.py.#{version}"
+#      owner "root"
+#      group "root"
+#      mode "0644"
+#      notifies :restart, resources(:service => "nova-compute"), :immediately
+#    end
+#  end
 end
