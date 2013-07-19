dc4c4e78ca903035d0d2531d69b10dc0a46dcd25
Jesse Nelson <spheromak@gmail.com>
2013-07-19 00:54:35 -0700

    bump metadata for dev (stackforge)

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "KT Cloudware, Inc."
 description       "Wrapper cookbook of rcb's nova cookbook"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.21"
+version           "2.0.1"
 recipe		        "compute", ""
 
 %w{ centos ubuntu }.each do |os|
