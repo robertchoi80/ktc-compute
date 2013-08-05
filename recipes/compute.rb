68a828e6286b9b970b94aa7a31f589518b2beca9
Wil Reichert <wil.reichert@kt.com>
2013-08-05 15:04:13 +0900

    configure proper remote service ip lookups

--- recipes/compute.rb
+++ recipes/compute.rb
@@ -6,19 +6,47 @@ class ::Chef::Recipe
   include ::Openstack
 end
 
-# search for control node and set all the necessary node attributes
-# so the nova service gets configured correctly
-control_node = config_by_role "os-compute-single-controller"
-if control_node
-  print "control node ip: #{control_node.ipaddress}"
-  node.default["memcached"]["listen"] = control_node.ipaddress
-  node.default["openstack"]["compute"]["rabbit"]["host"] = control_node.ipaddress
-  node.default["openstack"]["db"]["compute"]["host"] = control_node.ipaddress
+class Chef::Recipe
+  include KTCUtils
 end
+
+# search for node/s with rabbit recipe
+rabbit_servers = search_for "ktc-messaging"
+if rabbit_servers.length == 1
+  ip = get_interface_address("management", rabbit_servers.first)
+  node.default["openstack"]["compute"]["rabbit"]["host"] = ip
+elsif rabbit_servers.length > 1
+  node.default["openstack"]["compute"]["rabbit"]["ha"] = true
+  ips = []
+  rabbit_servers.each do |s|
+    ips << get_interface_address("management", s)
+  end
+  node.default["openstack"]["mq"]["servers"] = ips
+end
+
+# search for node with memcached recipe
+memcached_servers = search_for "infra-caching"
+if memcached_servers.length == 1
+  node.default["memcached"]["listen"] = get_interface_address("management", memcached_servers.first)
+elsif memcached_servers.length > 1
+  node.default["memcached"]["listen"] = get_interface_address("management", memcached_servers.first)
+  puts "#### TODO: deal with multiple memcached servers, just setting first for now"
+end
+
+# search for node with mysql recipe
+mysql_servers = search_for "os-ops-database"
+if mysql_servers.length == 1
+  node.default["openstack"]["db"]["compute"]["host"] = get_interface_address("management", mysql_servers.first)
+elsif
+  node.default["openstack"]["db"]["compute"]["host"] = get_interface_address("management", mysql_servers.first)
+  puts "#### TODO: deal with multiple mysql servers, just setting first for now"
+end
+
 
 chef_gem "chef-rewind"
 require 'chef/rewind'
 
+include_recipe "ktc-utils"
 include_recipe "openstack-compute::compute"
 # Add cgroup_device_acl option to /etc/libvirt/qemu.conf
 cookbook_file "/etc/libvirt/qemu.conf" do
@@ -38,7 +66,6 @@ if node["quantum"]["plugin"] == "lb"
 end
 
 # apply fixes for nova-compute
-include_recipe "ktc-utils"
 %w{ 2012.2.1+stable-20121212-a99a802e-0ubuntu1.4~cloud0 2012.2.3-0ubuntu2~cloud0 }.each do |version|
   if ::Chef::Recipe::Patch.check_package_version("nova-compute",version,node)
     template "/usr/share/pyshared/nova/network/quantumv2/api.py" do
