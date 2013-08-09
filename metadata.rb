e51a595c33514a84f2ff7e03fd8f7d3bc0c08af6
Hyunsun Moon <hyunsun.moon@gmail.com>
2013-08-09 16:08:29 +0900

    add compute-api recipe to metadata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,8 @@ maintainer        "KT Cloudware, Inc."
 description       "Wrapper cookbook of rcb's nova cookbook"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "2.0.1"
-recipe		        "compute", ""
+recipe		        "compute", "Include recipes required for compute node"
+recipe		        "compute-api", "Include recipes required for control node"
 
 %w{ centos ubuntu }.each do |os|
   supports os
