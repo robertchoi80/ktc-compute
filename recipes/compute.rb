820159ed194569e53216bd039e7a34f975a17ef9
Wil Reichert <wil.reichert@kt.com>
2013-08-20 23:42:48 +0900

    etcd updates

--- recipes/compute.rb
+++ recipes/compute.rb
@@ -14,6 +14,14 @@ end
 set_rabbit_servers "compute"
 set_memcached_servers
 set_database_servers "compute"
+set_service_endpoint "identity-api"
+set_service_endpoint "identity-admin"
+set_service_endpoint "image-registry"
+set_service_endpoint "image-api"
+set_service_endpoint "network-api"
+set_service_endpoint "compute-api"
+set_service_endpoint "compute-ec2-api"
+set_service_endpoint "compute-ec2-admin"
 
 include_recipe "ktc-utils"
 include_recipe "openstack-compute::compute"
