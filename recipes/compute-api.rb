3f01c796aa1bad4b519620f2fa0b3b591a58e0c7
Wil Reichert <wil.reichert@kt.com>
2013-08-20 14:07:07 +0900

    register_member name update

--- recipes/compute-api.rb
+++ recipes/compute-api.rb
@@ -7,13 +7,13 @@ class Chef::Recipe
 end
 
 d1 = get_openstack_service_template(get_interface_address("management"), "8774")
-register_service("compute-api", d1)
+register_member("compute-api", d1)
 
 d2 = get_openstack_service_template(get_interface_address("management"), "8773")
-register_service("compute-ec2-api", d2)
+register_member("compute-ec2-api", d2)
 
 d3 = get_openstack_service_template(get_interface_address("management"), "8773")
-register_service("compute-ec2-admin", d3)
+register_member("compute-ec2-admin", d3)
 
 set_rabbit_servers "compute"
 set_memcached_servers
