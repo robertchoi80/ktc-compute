55940ee5c2f85923069c277fa10205f51135835b
Jesse Nelson <spheromak@gmail.com>
2013-10-22 01:03:45 -0700

    fix error in vncproxy config.
    When using chef_rewind to overwrite a resource you need to make sure you
    pass all variables into the variable property of the resource that we
    defined in the base resource

--- recipes/nova-common.rb
+++ recipes/nova-common.rb
@@ -4,13 +4,8 @@ require 'chef/rewind'
 include_recipe "ktc-compute::source_install"
 include_recipe "openstack-compute::nova-common"
 
-vnc_server_listen = node["openstack"]["compute"]["vncserver_listen"]
 rewind :template => "/etc/nova/nova.conf" do
   cookbook "ktc-compute"
-  variables(
-    :vncserver_listen => vnc_server_listen
-  )
-
 end
 
 cookbook_file "/etc/nova/policy.json" do
