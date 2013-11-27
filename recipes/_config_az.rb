21687a8ec65df0441bea04a18f67027dbcf524b2
Wil Reichert <wil.reichert@kt.com>
2013-11-27 21:00:37 +0900

    fix oopses

--- recipes/_config_az.rb
+++ recipes/_config_az.rb
@@ -3,17 +3,18 @@
 az = node[:openstack][:availability_zone]
 
 # if az is not set skip all this
-if not az.nil? do
+unless az.nil?
   # check if az exists
   bash "configure_availability_zone" do
     user "root"
+    cwd "/root"
     code <<-EOH
       source /root/openrc
       /usr/local/bin/nova aggregate-list | grep #{az}
       [[ $? -ne 0 ]] && /usr/local/bin/nova aggregate-create #{az} #{az}
       /usr/local/bin/nova aggregate-details #{az} | grep #{node[:fqdn]}
       [[ $? -ne 0 ]] && /usr/local/bin/nova aggregate-add-host #{az} #{node[:fqdn]}
-      touch /var/chef/cache/.az_configured
+      touch /root/.az_configured
     EOH
     not_if { ::File.exists?("/var/chef/cache/.az_configured") }
   end
